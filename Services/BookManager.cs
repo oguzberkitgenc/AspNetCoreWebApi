af7e188dae58abc53960ea923f0d4407cd9d54fb
Oğuz Berkit Genç <58976917+ebrarcagla@users.noreply.github.com>
2024-12-15 02:54:21 +0300

    NLog

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -7,16 +7,16 @@ namespace Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _manager;
-        public BookManager(IRepositoryManager manager)
+        private readonly ILoggerService _logger;
+        public BookManager(IRepositoryManager manager, 
+            ILoggerService logger)
         {
             _manager = manager;
+            _logger = logger;
         }
 
         public Book CreateOneBook(Book book)
         {
-            if (book is null)
-                throw new ArgumentNullException(nameof(book));
-
             _manager.Book.CreateOneBook(book);
             _manager.Save();
             return book;
@@ -26,7 +26,11 @@ namespace Services
             //check entity
             var entity =_manager.Book.GetOneBookById(id,trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id: {id} could not found.");
+            {
+                string message = $"The book with id: {id} coul not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
 
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
@@ -47,10 +51,14 @@ namespace Services
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id: {id} could not found.");
+            {
+                string message = $"Book with id: {id} could not found.";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
 
             // check params
-            if(book is null)
+            if (book is null)
                 throw new ArgumentNullException(nameof(book));
 
             entity.Title = book.Title;
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,10 @@
+using NLog;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
@@ -13,7 +15,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
-
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
