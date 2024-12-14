69d439f3aee0b5ab2ef3d6444717add3f33dc4da
Oğuz Berkit Genç <58976917+ebrarcagla@users.noreply.github.com>
2024-12-15 02:07:34 +0300

    Add project files.

---
+++ Repositories/EFCore/config/BookConfig.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore.config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Karagöz ve Hacivar", Price = 15 },
+                new Book { Id = 2, Title = "Kürk Mantollu Madonna", Price = 150 },
+                new Book { Id = 3, Title = "Böyle söyledi zerdüşt", Price = 225 }
+                );
+        }
+    }
+}
